94fcfb2817c56df503fa23721279f8221988e57f
alvaroloes <alvarolopezespinosa@gmail.com>
2014-11-14 00:46:50 +0000

    + Added Gift model and a test request for a list of them

--- app/src/main/java/com/capstone/potlatch/SectionGifts.java
+++ app/src/main/java/com/capstone/potlatch/SectionGifts.java
@@ -6,14 +6,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.capstone.potlatch.models.Gift;
+import com.capstone.potlatch.net.JacksonRequest;
+import com.capstone.potlatch.net.Net;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.util.List;
 
 public class SectionGifts extends Fragment {
 
@@ -35,25 +36,23 @@ public class SectionGifts extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_section_gifts, container, false);
 
-        final TextView mTextView = (TextView) v.findViewById(R.id.text);
+        String url = "https://10.0.2.2:8443/gifts"; // This is the equivalent to localhost
 
-        String url ="http://www.google.com";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        JacksonRequest<List<Gift>> req = new JacksonRequest<List<Gift>>(url, new TypeReference<List<Gift>>() {}, new Response.Listener<List<Gift>>() {
             @Override
-            public void onResponse(String response) {
-                mTextView.setText("Response is: "+ response.substring(0,500));
+            public void onResponse(List<Gift> response) {
+                System.out.println("SUCCESS");
+                System.out.println(response);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
+                System.out.println("ERROR");
+                error.printStackTrace();
             }
         });
 
-        RequestQueue queue = Volley.newRequestQueue(getActivity());
-        queue.add(stringRequest);
+        Net.addToQueue(req);
         return v;
     }
 
---
+++ app/src/main/java/com/capstone/potlatch/models/Gift.java
@@ -0,0 +1,20 @@
+package com.capstone.potlatch.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+/**
+ * Created by alvaro on 13/11/14.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Gift {
+    public long id;
+    public String title;
+    public String description;
+    public long giftChainId;
+    public String giftChainName;
+//    public User user;
+    public List<Long> touchedByUserIds;
+    public List<Long> markedInappropriateByUserIds;
+}
