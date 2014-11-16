bd360408f5c99db6706b2090ffefcdc09eff0551
alvaroloes <alvarolopezespinosa@gmail.com>
2014-11-16 20:44:12 +0000

    + Gifts section data is now properly fetched (images remaining though)

--- app/src/main/java/com/capstone/potlatch/SectionGifts.java
+++ app/src/main/java/com/capstone/potlatch/SectionGifts.java
@@ -6,23 +6,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.capstone.potlatch.adapters.GiftsAdapter;
+import com.capstone.potlatch.base.Routes;
 import com.capstone.potlatch.models.Gift;
 import com.capstone.potlatch.net.JacksonRequest;
 import com.capstone.potlatch.net.Net;
 import com.fasterxml.jackson.core.type.TypeReference;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SectionGifts extends Fragment {
+    private List<Gift> gifts = new ArrayList<Gift>();
+    private boolean dataHasBeenLoaded = false;
+
+    // UI members
+    private GiftsAdapter mAdapter;
 
     public static SectionGifts newInstance() {
         SectionGifts fragment = new SectionGifts();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_TEXT, text);
-//        fragment.setArguments(args);
+/*        Bundle args = new Bundle();
+        args.putString(ARG_TEXT, text);
+        fragment.setArguments(args);
+*/
         return fragment;
     }
 
@@ -34,27 +44,43 @@ public class SectionGifts extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_section_gifts, container, false);
-
-        String url = "https://10.0.2.2:8443/gifts"; // This is the equivalent to localhost
-
-        JacksonRequest<List<Gift>> req = new JacksonRequest<List<Gift>>(url, new TypeReference<List<Gift>>() {}, new Response.Listener<List<Gift>>() {
-            @Override
-            public void onResponse(List<Gift> response) {
-                System.out.println("SUCCESS");
-                System.out.println(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println("ERROR");
-                error.printStackTrace();
-            }
-        });
-
-        Net.addToQueue(req);
+        ListView v = (ListView) inflater.inflate(R.layout.fragment_section_gifts, container, false);
+        mAdapter = new GiftsAdapter(getActivity(), gifts);
+        v.setAdapter(mAdapter);
         return v;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
 
+        if (!dataHasBeenLoaded) {
+            loadPageData();
+        }
+    }
+
+    private void loadPageData() {
+        String url = Routes.urlFor(Routes.GIFTS_PATH);
+        JacksonRequest<List<Gift>> req = new JacksonRequest<List<Gift>>(url,
+                                                                        new TypeReference<List<Gift>>() {},
+                                                                        new RequestSuccessListener(),
+                                                                        new RequestErrorListener());
+        Net.addToQueue(req);
+    }
+
+    class RequestSuccessListener implements Response.Listener<List<Gift>> {
+        @Override
+        public void onResponse(List<Gift> response) {
+            gifts.addAll(response);
+            dataHasBeenLoaded = true;
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    class RequestErrorListener implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            error.printStackTrace();
+        }
+    }
 }
---
+++ app/src/main/java/com/capstone/potlatch/adapters/GiftsAdapter.java
@@ -0,0 +1,40 @@
+package com.capstone.potlatch.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.capstone.potlatch.R;
+import com.capstone.potlatch.models.Gift;
+import com.capstone.potlatch.utils.ViewHolder;
+
+import java.util.List;
+
+/**
+ * Created by alvaro on 16/11/14.
+ */
+public class GiftsAdapter extends ArrayAdapter<Gift> {
+    public GiftsAdapter(Context context, List<Gift> gifts) {
+        super(context, 0, gifts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewGroup v = (ViewGroup) convertView;
+
+        if (v == null) {
+            v = (ViewGroup) View.inflate(getContext(), R.layout.partial_gift_row, null);
+        }
+
+        Gift g = getItem(position);
+
+        ((TextView) ViewHolder.get(v, R.id.gift_title)).setText(g.title);
+        ((TextView) ViewHolder.get(v, R.id.gift_chain_name)).setText(g.giftChainName);
+        ((TextView) ViewHolder.get(v, R.id.gift_description)).setText(g.description);
+        ((TextView) ViewHolder.get(v, R.id.gift_touches_count)).setText(String.valueOf(g.touchedByUserIds.size()));
+
+        return v;
+    }
+}
