e640d4d8ed42bc6892f19f1f60ed0b04171adab2
alvaroloes <alvarolopezespinosa@gmail.com>
2014-11-27 01:12:27 +0000

    - The gift data is collected and sent to the server. Still remains the image

--- app/src/main/java/com/capstone/potlatch/ActivityCreateUpdateGift.java
+++ app/src/main/java/com/capstone/potlatch/ActivityCreateUpdateGift.java
@@ -3,12 +3,20 @@ package com.capstone.potlatch;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.NavUtils;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.NetworkImageView;
 import com.capstone.potlatch.base.BaseActivity;
 import com.capstone.potlatch.base.Routes;
 import com.capstone.potlatch.base.State;
@@ -22,10 +30,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.util.List;
 
 
 public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogin.OnLoginListener {
     private static final String TAG_LOGIN = "ActivityCreateGift - TAG_LOGIN";
+
+    private List<GiftChain> giftChains;
+    private TextView mGiftChain;
+    private Button mSelectGiftChainButton;
+    private TextView mGiftTitle;
+    private TextView mGiftDescription;
+    private NetworkImageView mGiftImage;
+    private CheckBox mNoGiftChainCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +52,50 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
             DialogLogin.open(getFragmentManager(), TAG_LOGIN);
         }
 
+        mGiftTitle = (TextView) findViewById(R.id.gift_title);
+        mGiftImage = (NetworkImageView) findViewById(R.id.gift_image);
+        mGiftDescription = (TextView) findViewById(R.id.gift_description);
+        mGiftChain = (TextView) findViewById(R.id.gift_chain);
+        mSelectGiftChainButton = (Button) findViewById(R.id.select_gift_chain_button);
+
+        mNoGiftChainCheck = (CheckBox) findViewById(R.id.no_gift_chain_check);
+        mNoGiftChainCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mGiftChain.setEnabled(!isChecked);
+                mSelectGiftChainButton.setEnabled(!isChecked);
+            }
+        });
+
+        Button saveButton = (Button) findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDataAndSendGift();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+//        if (giftChains == null) {
+//            String url = Routes.urlFor(Routes.GIFTS_CHAIN_PATH);
+//            AuthRequest<List<GiftChain>> req = new AuthRequest<>(Request.Method.GET, url,
+//                    new TypeReference<List<GiftChain>>() {},
+//                    new GiftChainRequestListener(),
+//                    getErrorListener(true));
+//            req.setTag(this);
+//            Net.addToQueue(req);
+//        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        Net.getQueue().cancelAll(this);
+        super.onStop();
     }
 
     @Override
@@ -67,6 +128,93 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
         if (!success) {
             finish();
         }
+    }
+
+    private void getDataAndSendGift() {
+        boolean error = false;
+        String giftTitle = String.valueOf(mGiftTitle.getText());
+        String giftDescription = String.valueOf(mGiftDescription.getText());
+        String giftChainName = String.valueOf(mGiftChain.getText());
+        Long giftChainId = (Long) mGiftChain.getTag();
+        boolean thereMustBeAGiftChain = !mNoGiftChainCheck.isChecked();
+
+        // Check for errors
+
+        if (TextUtils.isEmpty(giftTitle)) {
+            mGiftTitle.setError("Please insert a Gift title");
+            error = true;
+        }
+
+        if (TextUtils.isEmpty(giftDescription)) {
+            mGiftDescription.setError("Please insert a Gift description");
+            error = true;
+        }
+
+        if (thereMustBeAGiftChain && TextUtils.isEmpty(giftChainName)) {
+            mGiftChain.setError("Please insert or select a Gift chain");
+            error = true;
+        }
+
+        if (error) {
+            return;
+        }
+
+        // Create the gift chain
+
+        GiftChain giftChain = null;
+        if (thereMustBeAGiftChain) {
+            giftChain = new GiftChain();
+            if (giftChainId != null) {
+                giftChain.id = giftChainId;
+            } else {
+                giftChain.name = giftChainName;
+            }
+        }
+
+        // Create the gift
+
+        Gift gift = new Gift();
+        gift.giftChain = giftChain;
+        gift.title = giftTitle;
+        gift.description = giftDescription;
+
+        // Get the image
+        //...
+
+        // Send the gift
+        File image = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/baobab.jpg");
+        sendGift(gift, image);
+    }
+
+    void sendGift(Gift gift, File image) {
+        String jsonGift = null;
+        try {
+            jsonGift = new ObjectMapper().writeValueAsString(gift);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        String url = Routes.urlFor(Routes.GIFTS_PATH);
+        AuthMultiPartRequest<Gift> req = new AuthMultiPartRequest<>(Request.Method.POST, url, Gift.class,
+                new Response.Listener<Gift>() {
+                    @Override
+                    public void onResponse(Gift response) {
+                        Log.d("AAAAAAAAAA", "GIFT CREADO GUAY");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("AAAAAAAAAA", "GIFT NO CREADO");
+                    }
+                }
+        );
+
+        req.addStringUpload("gift", jsonGift);
+        req.addFileUpload("image", image);
+        req.setTag(this);
+        Net.addToQueue(req);
     }
 
     private void testSendRequest() {
@@ -106,5 +254,14 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
 
 
         Net.addToQueue(req);
+    }
+
+
+    class GiftChainRequestListener implements Response.Listener<List<GiftChain>>
+    {
+        @Override
+        public void onResponse(List<GiftChain> response) {
+
+        }
     }
 }
