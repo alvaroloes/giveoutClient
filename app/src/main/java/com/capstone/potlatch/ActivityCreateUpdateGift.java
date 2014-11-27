cebff219033a53d8a9f249e983c06f328b12d595
alvaroloes <alvarolopezespinosa@gmail.com>
2014-11-27 21:22:28 +0000

    + Now the photos of the Gift can be selected from gallery or taken with the camera

--- app/src/main/java/com/capstone/potlatch/ActivityCreateUpdateGift.java
+++ app/src/main/java/com/capstone/potlatch/ActivityCreateUpdateGift.java
@@ -1,22 +1,26 @@
 package com.capstone.potlatch;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.NetworkImageView;
 import com.capstone.potlatch.base.BaseActivity;
 import com.capstone.potlatch.base.Routes;
 import com.capstone.potlatch.base.State;
@@ -29,19 +33,31 @@ import com.capstone.potlatch.net.requests.AuthMultiPartRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 
 public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogin.OnLoginListener {
+    private static final String STATE_IMAGE_URI = "STATE_IMAGE_URI";
+
     private static final String TAG_LOGIN = "ActivityCreateGift - TAG_LOGIN";
+    private static final int IMAGE_CAPTURE_REQUEST = 0;
+    private static final int IMAGE_PICK_REQUEST = 1;
 
     private List<GiftChain> giftChains;
+    private Uri tempImageUri;
+
     private TextView mGiftChain;
     private Button mSelectGiftChainButton;
     private TextView mGiftTitle;
     private TextView mGiftDescription;
-    private NetworkImageView mGiftImage;
+    private ImageView mGiftImage;
     private CheckBox mNoGiftChainCheck;
 
     @Override
@@ -53,7 +69,7 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
         }
 
         mGiftTitle = (TextView) findViewById(R.id.gift_title);
-        mGiftImage = (NetworkImageView) findViewById(R.id.gift_image);
+        mGiftImage = (ImageView) findViewById(R.id.gift_image);
         mGiftDescription = (TextView) findViewById(R.id.gift_description);
         mGiftChain = (TextView) findViewById(R.id.gift_chain);
         mSelectGiftChainButton = (Button) findViewById(R.id.select_gift_chain_button);
@@ -67,6 +83,22 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
             }
         });
 
+        ImageButton takePhotoButton = (ImageButton) findViewById(R.id.image_from_camera_button);
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCameraIntent();
+            }
+        });
+
+        ImageButton pickPhotoButton = (ImageButton) findViewById(R.id.image_from_gallery_button);
+        pickPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchPickImageIntent();
+            }
+        });
+
         Button saveButton = (Button) findViewById(R.id.save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +106,25 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
                 getDataAndSendGift();
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mGiftImage.getTag() != null) {
+            outState.putString(STATE_IMAGE_URI, String.valueOf(mGiftImage.getTag()));
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String savedUriString = savedInstanceState.getString(STATE_IMAGE_URI);
+        if (savedUriString != null) {
+            Uri imageUri = Uri.parse(savedUriString);
+            mGiftImage.setImageURI(imageUri);
+            mGiftImage.setTag(imageUri);
+        }
     }
 
     @Override
@@ -107,9 +158,6 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         switch(id) {
@@ -155,12 +203,17 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
             error = true;
         }
 
+        Uri giftImageUri = (Uri) mGiftImage.getTag();
+        if (giftImageUri == null) {
+            Toast.makeText(this, "Please take or select a photo for this Gift",Toast.LENGTH_LONG).show();
+            error = true;
+        }
+
         if (error) {
             return;
         }
 
         // Create the gift chain
-
         GiftChain giftChain = null;
         if (thereMustBeAGiftChain) {
             giftChain = new GiftChain();
@@ -172,17 +225,13 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
         }
 
         // Create the gift
-
         Gift gift = new Gift();
         gift.giftChain = giftChain;
         gift.title = giftTitle;
         gift.description = giftDescription;
 
-        // Get the image
-        //...
-
         // Send the gift
-        File image = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/baobab.jpg");
+        File image = new File(giftImageUri.getPath());
         sendGift(gift, image);
     }
 
@@ -200,21 +249,63 @@ public class ActivityCreateUpdateGift extends BaseActivity implements DialogLogi
                 new Response.Listener<Gift>() {
                     @Override
                     public void onResponse(Gift response) {
-                        Log.d("AAAAAAAAAA", "GIFT CREADO GUAY");
+                        finish();
                     }
                 },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("AAAAAAAAAA", "GIFT NO CREADO");
-                    }
-                }
+                getErrorListener(true)
         );
 
         req.addStringUpload("gift", jsonGift);
         req.addFileUpload("image", image);
         req.setTag(this);
         Net.addToQueue(req);
+    }
+
+    private void createTempImageUri() {
+        try {
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File imageFile = File.createTempFile("giftImage", ".jpg", storageDir);
+            tempImageUri = Uri.fromFile(imageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void launchCameraIntent() {
+        createTempImageUri();
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        i.putExtra(MediaStore.EXTRA_OUTPUT, tempImageUri);
+        startActivityForResult(i, IMAGE_CAPTURE_REQUEST);
+    }
+
+    private void launchPickImageIntent() {
+        createTempImageUri();
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, IMAGE_PICK_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        switch(requestCode) {
+            case IMAGE_CAPTURE_REQUEST:
+            break;
+            case IMAGE_PICK_REQUEST:
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    OutputStream out = new FileOutputStream(new File(tempImageUri.getPath()));
+                    IOUtils.copy(in, out);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+        // TODO: Decode the image in background
+        mGiftImage.setImageURI(tempImageUri);
+        mGiftImage.setTag(tempImageUri);
     }
 
     private void testSendRequest() {
